aaf3de592e4ab0d1e7daba3546851bd4d8736e08
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-15 01:31:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-  case array.length
-   when <= 2
-     array.join(" and ")
-   when >= 2
-     array.join(" and ")
-   end
+  if array.length <= 2
+    array.join(" and ")
+  else
+    last_item = array.pop
+    array.join(", ") + "and " + last_item
 end
